fe777811770d1e0db3254fd84574005773add95d
lixin <975788767@qq.com>
2020-12-15 15:17:17 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<!--
+ * @Author: 李鑫
+ * @Date: 2020-12-15 14:48:41
+ * @LastEditors: 李鑫
+ * @LastEditTime: 2020-12-15 15:14:49
+-->
+<template></template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
